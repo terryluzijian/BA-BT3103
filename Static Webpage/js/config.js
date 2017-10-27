583af942afa86abbee719e2d8a2b12ec2819878d
Terry Lu Zijian <terrylu0713@gmail.com>
2017-10-27 18:39:36 +0800

    Update Necessary Code for Web Page

---
+++ Static Webpage/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-1_REQK9vg4q', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2u5um5tup867daopkoaboq1sns', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
