e5864c7ced9ed4e9f23f83e862f077b4c02a6db8
Terry Lu Zijian <terryluzijian@gmail.com>
2017-11-09 13:15:22 +0800

    Added Few More Pages

--- static/js/cognito-auth.js
+++ static/js/cognito-auth.js
@@ -127,7 +127,7 @@ var WildRydes = window.WildRydes || {};
         signin(email, password,
             function signinSuccess() {
                 console.log('Successfully Logged In');
-                window.location.href = 'ride.html';
+                window.location.href = 'index.html';
             },
             function signinError(err) {
                 alert(err);
