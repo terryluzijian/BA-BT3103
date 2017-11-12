df55c236d6155ba5a3e432daf133ff6c8ac269c5
Sheng Yu <shengyu9705@gmail.com>
2017-11-12 14:34:26 +0800

    _bfs modify

--- api/scripts/InterbuildingAPI.py
+++ api/scripts/InterbuildingAPI.py
@@ -5,8 +5,8 @@ from BasicAPI import BasicAPI
 class InterbuildingAPI(BasicAPI):
 
     type_name = 'interbuilding'
-    building_connection = json.load(open('data/BUILDING_CONNECTION.json', 'r'))
-    building_geocode = json.load(open('data/BUILDING_GEOCODE.json', 'r'))
+    building_connection = json.load(open('./data/BUILDING_CONNECTION.json', 'r'))
+    building_geocode = json.load(open('./data/BUILDING_GEOCODE.json', 'r'))
 
     def __init__(self, user_lat, user_lon, search_distance=BasicAPI.proximity_threshold):
         super(InterbuildingAPI, self).__init__(user_lat, user_lon, search_distance)
@@ -16,7 +16,7 @@ class InterbuildingAPI(BasicAPI):
         self.get_buildings_nearby()
 
     def get_buildings(self):
-        self.buidlings.extend([{
+        self.buildings.extend([{
             'index': i,
             'lat': self.building_geocode[i]['lat'],
             'lon': self.building_geocode[i]['lng'],
@@ -28,19 +28,33 @@ class InterbuildingAPI(BasicAPI):
             self.get_buildings()
         buildings_nearby = sorted(
             list(filter(lambda building_new_dict: building_new_dict['dist'] <= self.search_distance,
-                        map(lambda building_dct:
+                        map(lambda building_dict:
                             {
-                                'lat': building_dct['lat'],
-                                'lon': building_dct['lon'],
-                                'name': building_dct['name'],
-                                'index': building_dct['index'],
+                                'lat': building_dict['lat'],
+                                'lon': building_dict['lon'],
+                                'name': building_dict['name'],
+                                'index': building_dict['index'],
                                 'dist': self.get_distance((self.user_lat, self.user_lon),
-                                                          (building_dct['lat'], building_dct['lon']))
+                                                          (building_dict['lat'], building_dict['lon']))
                             },
                             self.buildings))),
             key=lambda building_final_dict: building_final_dict['dist'])
-        self.buildings_nearby = buildings_nearby
+        self.buildings_nearby = buildings_nearby[:5]
 
     def get_connected_buildings_nearby(self):
         if len(self.buildings_nearby) == 0:
             self.get_buildings_nearby()
+        connected_buildings_nearby = [{building_dict['index']:[buildings
+                                                               for buildings in self._bfs(self.building_connection, building_dict['index'])
+                                                              ]} for building_dict in self.buildings_nearby]
+        return connected_buildings_nearby
+    
+    def _bfs(self, graph, start):
+        visited, queue = set(), [start]
+        while queue:
+            vertex = queue.pop(0)
+            if vertex not in visited:
+                visited.add(vertex)
+                queue.extend(set([i for i in range(len(graph[vertex])) if graph[vertex][i] == 1]) - visited)
+        return visited
+    
